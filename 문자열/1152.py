ff1df5b70cb4efd5d31699a956e4335a4be0825b
SoMin <thals0107@naver.com>
2022-07-17 16:06:51 +0900

    Update 1152

--- 문자열/1152.py
+++ 문자열/1152.py
@@ -1,9 +1,9 @@
-a = input()
-cnt = 1
-for i in a:
-  if i==" ":
-    cnt= cnt + 1
-print(cnt)
+# a = input()
+# cnt = 1
+# for i in a:
+#   if i==" ":
+#     cnt= cnt + 1
+# print(cnt)
 
-# word = input().split()
-# print(len(word))
+word = input().split()
+print(len(word))
