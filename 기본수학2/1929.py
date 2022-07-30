9fa79733ffcd221a4fc191f30ed4023edbe2b566
SoMin <thals0107@naver.com>
2022-07-30 14:27:52 +0900

    Update

--- 기본수학2/1929.py
+++ 기본수학2/1929.py
@@ -1,10 +1,22 @@
 # 시간 초과
+# a,b= map(int, input().split())
+# for i in range(a,b+1):
+#   cnt = 0
+#   if i > 1:
+#     for j in range(2,i):
+#       if i % j == 0:
+#         cnt += 1
+#     if cnt == 0:
+#       print(i)
+
+
+
 a,b= map(int, input().split())
 for i in range(a,b+1):
-  cnt = 0
-  if i > 1:
-    for j in range(2,i):
-      if i % j == 0:
-        cnt += 1
-    if cnt == 0:
-      print(i)
+  if i == 1:
+    continue
+  for j in range(2,int(i**0.5)+1):
+    if i % j == 0:
+      break
+  else:
+    print(i)
