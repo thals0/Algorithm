65eb32d5c9fba2983b3d93359543466c484444f2
SoMin <thals0107@naver.com>
2022-10-08 17:45:28 +0900

    solved 10814

--- 정렬/1181.py
+++ 정렬/1181.py
@@ -4,7 +4,7 @@ for i in range(n):
   arr.append(input())
 set_arr = set(arr) # set으로 변환해서 중복값을 제
 arr = list(set_arr) # 다시 list로 변환
-arr.sort()
+# arr.sort()
 arr.sort(key=len)
 for i in arr:
   print(i)
