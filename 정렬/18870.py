8d6a2ebdc5647663eb21fcea0183a613e05a66f0
SoMin <thals0107@naver.com>
2022-10-10 10:36:08 +0900

    Update 18870

--- 정렬/18870.py
+++ 정렬/18870.py
@@ -2,11 +2,14 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 arr2 = sorted(list(set(arr)))
+print(arr2)
 
 # 시간복잡도 초과
 # for i in arr:
 #   print(arr2.index(i), end='')
 
 dic = {arr2[i]: i for i in range(len(arr2))}
+print(dic)
+
 for i in arr:
-  print(dic[i], end='')
+  print(dic[i], end=' ')
