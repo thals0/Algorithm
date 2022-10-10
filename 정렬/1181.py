ead9d55b1381eea30181da55e58643175460143b
SoMin <thals0107@naver.com>
2022-10-10 10:05:11 +0900

    Update 1181

--- 정렬/1181.py
+++ 정렬/1181.py
@@ -2,9 +2,9 @@ n = int(input())
 arr = []
 for i in range(n):
   arr.append(input())
-set_arr = set(arr) # set으로 변환해서 중복값을 제
+set_arr = set(arr) # set으로 변환해서 중복값을 제거
 arr = list(set_arr) # 다시 list로 변환
-# arr.sort()
+arr.sort()
 arr.sort(key=len)
 for i in arr:
   print(i)
