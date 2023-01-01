8a64807ce19ab78c60a2e698caa003ddcadba6ab
SoMin <thals0107@naver.com>
2023-01-01 19:00:26 +0900

    solved 10828

---
+++ 자료구조1/10828.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+class Stack:
+  def __init__(self):
+    self.data=[]
+  def empty(self):
+    if len(self.data) == 0:
+      return 1
+    else:
+      return 0
+  def size(self):
+    return len(self.data)
+  def push(self, X):
+    self.data.append(X)
+  def pop(self):
+    if self.size() == 0:
+      return -1
+    else:
+      return self.data.pop()
+  def top(self):
+    if self.size() == 0:
+      return -1
+    else:
+      return self.data[-1]
+
+num = int(input())
+stack = Stack()
+for _ in range(num):
+  cmd = input().split()
+
+  op = cmd[0]
+  if op == 'push':
+    stack.push(cmd[1])
+  elif op == 'empty':
+    print(stack.empty())
+  elif op == 'pop':
+    print(stack.pop())
+  elif op == 'size':
+    print(stack.size())
+  elif op == 'top':
+    print(stack.top())
+  else:
+    print('err')
