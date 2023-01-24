62e77e650c0f1109d6e6d0487c7df7cb89116cf5
SoMin <thals0107@naver.com>
2023-01-24 19:04:01 +0900

    [Silver Ⅱ] Title: 로또, Time: 64 ms, Memory: 30616 KB, using DFS- Baekjoon

--- DFS & BFS/6603.py
+++ DFS & BFS/6603.py
@@ -1,13 +1,31 @@
-from itertools import combinations
+# from itertools import combinations
 
+# while True:
+#   num = list(map(int, input().split()))
+#   if len(num) == 1 and num[0] == 0:
+#     break
+#   items = num[1:]
+#   ans = list(combinations(items, 6))
+#   for i in ans:
+#     for j in i:
+#       print(j, end=' ')
+#     print()
+#   print()
+
+def dfs(start, depth):
+    if depth == 6:
+        for i in range(6):
+            print(combi[i], end=' ')
+        print()
+        return
+    for i in range(start, len(s)):
+        combi[depth] = s[i]
+        dfs(i + 1, depth + 1)
+combi = [0 for i in range(13)]
 while True:
-  num = list(map(int, input().split()))
-  if len(num) == 1 and num[0] == 0:
-    break
-  items = num[1:]
-  ans = list(combinations(items, 6))
-  for i in ans:
-    for j in i:
-      print(j, end=' ')
+    s = list(map(int, input().split()))
+    if s[0] == 0:
+        break
+    del s[0]
+    dfs(0, 0)
     print()
-  print()
