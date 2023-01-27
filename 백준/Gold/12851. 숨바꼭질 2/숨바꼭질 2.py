7fd88aec779f2d20819f1a9864ee3f0391d16bec
SoMin <thals0107@naver.com>
2023-01-27 12:00:56 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 692 ms, Memory: 37776 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+q = deque()
+q.append(n)
+
+visited = [0] * 100001
+visited[n] = 0
+
+ans_count = 0
+ans_way = 0
+while q:
+    x = q.popleft()
+    count = visited[x]
+    if x == k:
+        ans_count = count
+        ans_way += 1
+        continue
+
+    for nx in [x-1, x+1, 2*x]:
+        if 0 <= nx < 100001:
+            if visited[nx] == 0 or visited[nx] == visited[x] + 1:
+                q.append(nx)
+                visited[nx] = count + 1
+
+print(ans_count)
+print(ans_way)
