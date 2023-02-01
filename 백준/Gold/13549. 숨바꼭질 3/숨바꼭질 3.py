40f7d4b905b97419b4701c16f804fe3eb0e9385b
SoMin <thals0107@naver.com>
2023-02-01 11:03:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 104 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,24 +1,27 @@
-import sys
 from collections import deque
-
+import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-road = [0] * 100001
+n,k = map(int,input().split())
+MAX_NUM = 100000
+dist = [0] * (MAX_NUM+1)
 
-queue = deque()
-queue.append(N)
-road[N] = 1
-while queue:
-  x = queue.popleft()
-  if x == K:
-    print(road[x]-1)
-    break
-  for nx in (2*x, x-1, x+1):
-    if 0 <= nx < 100001 and road[nx] == 0:
-      if nx == 2*x:
-        road[nx] = road[x]
-        queue.appendleft(nx)
-      else:
-        road[nx] = road[x] + 1
-        queue.append(nx)
+def bfs():
+  q = deque()
+  q.append(n)
+  dist[n] = 1
+  while q:
+    x = q.popleft()
+    if x == k:
+      print(dist[x]-1)
+      break
+    for nx in (x-1, x+1, 2*x):
+      if 0 <= nx <= MAX_NUM and dist[nx]==0:
+        if nx == 2*x:
+          dist[nx] = dist[x]
+          q.appendleft(nx)
+        else:
+          dist[nx] = dist[x] + 1
+          q.append(nx)
+
+bfs()
