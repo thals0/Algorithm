02918f0b3605aba12c15c2507a1ad2dcb4e77f78
SoMin <thals0107@naver.com>
2023-02-02 16:29:19 +0900

    [Silver III] Title: 1로 만들기, Time: 488 ms, Memory: 46620 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,18 @@
-x = int(input())
-d = [0] * (10**6 + 1)
-# 다이나믹 프로그래밍 진행(보텀업)
-for i in range(2, x+1):
-  d[i] = d[i-1] + 1
-  if i%2==0:
-    d[i] = min(d[i], d[i//2]+1)
-  if i%3 ==0:
-    d[i] = min(d[i], d[i//3]+1)
+from collections import deque
+n = int(input())
+dist = [0]*(10**6+1)
 
-print(d[x])
+def bfs():
+  q = deque()
+  q.append(1)
+  while q:
+    x = q.popleft()
+    if x == n:
+      print(dist[x])
+      break
+    for nx in (3*x, 2*x, x+1):
+      if 0<= nx <= 10**6 and dist[nx] == 0:
+        dist[nx] = dist[x] + 1
+        q.append(nx)
+
+bfs()
