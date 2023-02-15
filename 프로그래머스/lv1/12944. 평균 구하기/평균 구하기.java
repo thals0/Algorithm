ce21b22da48df5a22496914d52fa47bdc6f8f18e
SoMin <thals0107@naver.com>
2023-02-15 13:24:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        for (int num: arr){
+            sum += num;
+        }
+        answer = sum / (double) arr.length;
+        return answer;
+    }
+}
