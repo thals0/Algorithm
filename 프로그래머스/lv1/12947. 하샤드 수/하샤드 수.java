225239ea9449a103a695579fdd7f451b69296daa
SoMin <thals0107@naver.com>
2023-02-18 12:50:16 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int tmp = x;
+        while(tmp > 0){
+            sum += tmp%10;
+            tmp /= 10;
+        }
+        if(x%sum == 0){
+            return true;
+        } else return false;
+    }
+}
