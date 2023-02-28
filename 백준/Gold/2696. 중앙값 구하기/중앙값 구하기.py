33c136c80eec5d267063f4f2d2fb8ed346817929
SoMin <thals0107@naver.com>
2023-02-28 09:55:46 +0900

    [Gold II] Title: 중앙값 구하기, Time: 2604 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,14 +1,14 @@
-from sys import stdin
-input = lambda: stdin.readline().rstrip()
+import sys
+input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-		n = int(input())
-		nums = []
-		for _ in range(n // 10 + 1):
-				nums += list(map(int, input().split()))
+t = int(input())
 
-		print(n // 2 + 1)
-		for i in range(1, len(nums) + 1, 2):
-						print(sorted(nums[:i])[i // 2], end=' ')
-		print()
+for _ in range(t):
+  n = int(input())
+  num = []
+  for _ in range(n//10+1):
+    num += list(map(int, input().split()))
+  print(n//2+1)
+  for i in range(1, len(num)+1, 2):
+    print(sorted(num[:i])[i//2], end=' ')
+  print()
