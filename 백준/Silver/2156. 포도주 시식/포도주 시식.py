346f5bf240a4890dfec9c9b490bb0ce1c2177f5b
SoMin <thals0107@naver.com>
2023-03-11 10:25:39 +0900

    [Silver I] Title: 포도주 시식, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0]*10001
+wines = [0]*10001
+for i in range(n):
+  wines[i] = int(input())
+
+dp[0] = wines[0]
+dp[1] = wines[0]+wines[1]
+dp[2] = max(dp[1], wines[0]+ wines[2], wines[1]+wines[2])
+
+for i in range(3, n):
+  dp[i] = max(dp[i-1], dp[i-2]+wines[i], dp[i-3]+ wines[i-1]+wines[i])
+
+# print(dp)
+# print(dp[n-1])
+print(max(dp))
