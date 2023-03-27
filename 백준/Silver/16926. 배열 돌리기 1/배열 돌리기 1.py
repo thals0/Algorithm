d605d053bc22d9c72cc90b69642aa811d2375b96
SoMin <thals0107@naver.com>
2023-03-27 17:24:07 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 100 ms, Memory: 38168 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,39 +1,33 @@
-import sys
-input = sys.stdin.readline
+from sys import stdin
+from collections import deque
 
-n,m,r = map(int,input().split())
+N, M, R = map(int, stdin.readline().split())
 
-data = [list(map(int,input().split())) for _ in range(n)]
+matrix = []
+answer = [[0]*M for _ in range(N)]
+deq = deque()
 
-for _ in range(r):
-  for i in range(min(n, m) // 2):
-    x, y = i, i
-    temp = data[x][y]
+for i in range(N):
+    matrix.append(list(stdin.readline().split()))
+
+loops = min(N, M) // 2
+for i in range(loops):
+    deq.clear()
+    deq.extend(matrix[i][i:M-i])
+    deq.extend([row[M-i-1] for row in matrix[i+1:N-i-1]])
+    deq.extend(matrix[N-i-1][i:M-i][::-1])
+    deq.extend([row[i] for row in matrix[i+1:N-i-1]][::-1])
     
-    for j in range(i + 1, n - i): 
-      x = j
-      prev_value = data[x][y]
-      data[x][y] = temp
-      temp = prev_value
+    deq.rotate(-R)
+    
+    for j in range(i, M-i):                 # 위쪽
+        answer[i][j] = deq.popleft()
+    for j in range(i+1, N-i-1):             # 오른쪽
+        answer[j][M-i-1] = deq.popleft()
+    for j in range(M-i-1, i-1, -1):           # 아래쪽
+        answer[N-i-1][j] = deq.popleft()  
+    for j in range(N-i-2, i, -1):           # 왼쪽
+        answer[j][i] = deq.popleft()    
 
-    for j in range(i + 1, m - i):
-      y = j
-      prev_value = data[x][y]
-      data[x][y] = temp
-      temp = prev_value
-
-    for j in range(i + 1, n - i):
-      x = n - j - 1
-      prev_value = data[x][y]
-      data[x][y] = temp
-      temp = prev_value
-
-    for j in range(i + 1, m - i):
-      y = m - j -1
-      prev_value = data[x][y]
-      data[x][y] = temp
-      temp = prev_value
-
-for i in data:
-  print(*i)
-
+for line in answer:
+    print(" ".join(line))
