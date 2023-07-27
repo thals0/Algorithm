32742608b363e832d1b219c393c5448b70765aea
SoMin <thals0107@naver.com>
2023-07-27 12:03:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,5 +1,2 @@
 def solution(phone_number):
-    answer = ''
-    answer += (len(phone_number)-4)*'*'
-    answer += phone_number[-4:]
-    return answer
+    return (len(phone_number)-4)*'*' + phone_number[-4:]
