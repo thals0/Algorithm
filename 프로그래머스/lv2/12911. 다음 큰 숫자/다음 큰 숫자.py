ce84fdf8a90ec3a6033cd2dcf17ee0ef208a8cfd
SoMin <thals0107@naver.com>
2023-07-31 11:34:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -4,5 +4,5 @@ def solution(n):
     while(True):
         n += 1
         bi = format(n, 'b')
-        if str(binaryNum).count('1') == str(bi).count('1'):
+        if binaryNum.count('1') == bi.count('1'):
             return n
