4e98a46b5655652d7c9a0d8cf961a67375ade582
SoMin <thals0107@naver.com>
2023-07-31 11:25:33 +0900

    [level 2] Title: 숫자의 표현, Time: 6.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        total = 0
+        for j in range(i, n+1):
+            total += j
+            if total == n:
+                answer += 1
+                break
+            if total > n:
+                break
+    return answer
