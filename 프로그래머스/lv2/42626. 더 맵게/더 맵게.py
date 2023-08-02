7e6f16ea2f40cee33d135f75ae6969a6bdcad7ac
SoMin <thals0107@naver.com>
2023-08-02 15:29:34 +0900

    [level 2] Title: 더 맵게, Time: 1839.69 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+from heapq import heapify, heappush, heappop
+def solution(scoville, K):
+    answer = 0
+    heapify(scoville)
+    while True:
+        if scoville[0] >= K:
+            break
+        if len(scoville) <= 1 and scoville[0] < K:
+            return -1
+        v1 = heappop(scoville)
+        v2 = heappop(scoville)
+        heappush(scoville, v1+(v2*2))
+        answer += 1
+    return answer
