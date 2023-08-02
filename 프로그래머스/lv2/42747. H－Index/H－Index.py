676285f8fa681425264d9874d45b89c30df13f69
SoMin <thals0107@naver.com>
2023-08-02 15:49:02 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,15 +1,7 @@
 def solution(citations):
-    answer = 0
-    listAnswer = []
-    setCitations = sorted(list(set(citations)),reverse=True)
-    maxSet = max(setCitations)
-    citations.sort(reverse=True)
-    dictCitations = {x:0 for x in range(maxSet,-1,-1)}
-    for i in citations:
-        for l in dictCitations.keys():
-            if l <= i:
-                dictCitations[l] += 1
-    for x, y in dictCitations.items():
-        if x <= y:
-            return x
-    return answer
+    citations.sort()
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
