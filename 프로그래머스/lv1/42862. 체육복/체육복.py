e48c7f65489ca42ed416197b21c2e4094cc36281
SoMin <thals0107@naver.com>
2023-08-03 09:42:28 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,26 +1,16 @@
 def solution(n, lost, reserve):
-    answer = 0
-    students, rent  = [1]*(n+1), [0]*(n+2)
-    students[0],rent[0] = 3,3
-    
-    for lo in lost:
-        students[lo] = 0
-    for re in reserve:
-        rent[re] = 1
-        
-    for i in range(1,n+1):
-        if students[i] == 0:
-            if rent[i] == 1: #본인이 여분을 가지고 있었을 경우
-                rent[i] = 0 
-                students[i] =1
-            elif rent[i-1] == 1 and students[i-1] != 0: 
-                rent[i-1] = 0
-                students[i] =1
-            elif rent[i+1] == 1 and students[i+1] != 0:
-                rent[i+1] = 0
-                students[i] =1
-                
-    for student in students:
-        if student == 1:
-            answer += 1
-    return answer
+    lst = sorted(lost)
+    reserve.sort()
+    for i in lst:
+        if i in reserve:
+            reserve.remove(i)
+            lost.remove(i)
+    lst2 = sorted(lost)
+    for i in lst2:
+        if i-1 in reserve:
+            reserve.remove(i-1)
+            lost.remove(i)
+        elif i+1 in reserve:
+            reserve.remove(i+1)
+            lost.remove(i)
+    return n-len(lost)
