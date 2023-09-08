128b28a45a2b0c98560eacd5f2dfa3a3290fc573
SoMin <thals0107@naver.com>
2023-09-08 14:16:27 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,14 @@
+def solution(name, yearning, photo):
+    answer = []
+    dict = {}
+    
+    for i in range(len(name)):
+        dict[name[i]] = yearning[i]
+    
+    for i in photo:
+        score = 0
+        for j in i:
+            if j in dict:
+                score += dict[j]
+        answer.append(score)
+    return answer
