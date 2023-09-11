daf8d3766dc6a6e8804c964c0b9d624ec7cbc3fc
SoMin <thals0107@naver.com>
2023-09-11 12:45:18 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.27 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,10 @@
+def solution(numer1, denom1, numer2, denom2):
+    numer = denom1*numer2 + denom2*numer1
+    denom = denom1*denom2
+    d = denom
+    for i in range(d, 1, -1):
+        if numer%i == 0 and denom%i  == 0:
+            numer = numer//i
+            denom = denom//i
+    answer = [numer, denom]
+    return answer
