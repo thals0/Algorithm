41b073788726d19020318327be42da9ac43827c2
SoMin <thals0107@naver.com>
2023-10-07 14:14:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,7 @@
 def solution(s):
-    answer = []
-    arr = list(map(int, s.split(' ')))
-    arr.sort()
-    answer.append(str(arr[0]))
-    answer.append(str(arr[len(arr)-1]))
-    return " ".join(answer)
+    answer = ''
+    arr = list(map(int, s.split()))
+    answer += str(min(arr))
+    answer += ' '
+    answer += str(max(arr))
+    return answer
