13fba37e614c513edb89943b19dd2fe4f22e95ed
SoMin <thals0107@naver.com>
2023-10-08 09:06:24 +0900

    [level 1] Title: 폰켓몬, Time: 1.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,14 @@
 def solution(nums):
-    n = len(nums)//2
-    if n <= len(set(nums)) :
-        return n
+    answer = 0
+    dict = {}
+    for i in nums:
+        if i not in dict:
+            dict[i] = 1
+        else:
+            dict[i] += 1
+    n = len(nums)/2
+    if len(dict) >= n:
+        answer = n
     else:
-        return len(set(nums))
+        answer = len(dict)
+    return answer
