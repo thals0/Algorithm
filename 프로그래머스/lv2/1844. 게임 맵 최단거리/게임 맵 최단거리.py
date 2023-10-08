6726a0f9d99ffc2c210661f0826ffd6e7ff394df
SoMin <thals0107@naver.com>
2023-10-08 11:24:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,27 +1,29 @@
 from collections import deque
 
-def solution(maps):
-    dx = [-1,1,0,0]
-    dy = [0,0,1,-1]
-    
+def solution(maps):   
     n = len(maps)
     m = len(maps[0])
-
+    
+    graph = [[0]*m for _ in range(n)]
+    
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+    
     q = deque()
     q.append((0,0))
     
     while q:
-        x, y = q.popleft()
+        x,y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx <0 or nx >=n or ny <0 or ny>=m:
                 continue
-            if maps[nx][ny] == 0:
-                continue
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
+            if maps[nx][ny] == 1 and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
                 q.append((nx,ny))
-    if maps[n-1][m-1] == 1: 
+    
+    if graph[n-1][m-1] == 0:
         return -1
-    return maps[n-1][m-1]
+    else: 
+        return graph[n-1][m-1] + 1
