d790169cb8840caef1534eccf03fac4f2902ab3b
SoMin <thals0107@naver.com>
2023-11-21 13:46:19 +0900

    [level 3] Title: 등굣길, Time: 3.77 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -6,7 +6,7 @@ def solution(m, n, puddles):
         for j in range(1,m+1):
             if [j,i] in puddles: continue
             if i-1 >=1 and [j,i-1] not in puddles:
-                dp[i][j] += dp[i-1][j] % 1000000007
+                dp[i][j] += dp[i-1][j]
             if j-1 >= 1 and [j-1,i] not in puddles:
-                dp[i][j] += dp[i][j-1] % 1000000007
+                dp[i][j] += dp[i][j-1]
     return dp[-1][-1] % 1000000007
