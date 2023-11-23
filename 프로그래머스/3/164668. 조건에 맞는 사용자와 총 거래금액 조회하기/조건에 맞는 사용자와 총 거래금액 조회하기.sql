7eb1cbb429dbbe74cde279fc3493b72bf6677254
SoMin <thals0107@naver.com>
2023-11-23 11:45:03 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT u.USER_ID, u.NICKNAME, SUM(PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD b left join USED_GOODS_USER u
+on b.WRITER_ID= u.USER_ID
+where STATUS = 'DONE'
+group by u.USER_ID, u.NICKNAME
+having TOTAL_SALES >= 700000
+order by TOTAL_SALES
