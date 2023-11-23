b3d72da632e8637d70723d7c8126df5931204d49
SoMin <thals0107@naver.com>
2023-11-23 16:48:42 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 588.77 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<n+1; i++){
+            for (int j=1; j<n+1; j++){
+                if (i*j==n){
+                    if (i == j){
+                        answer *= 2;
+                        answer += 1;
+                        return answer;
+                    } else if (i > j){
+                        answer *= 2;
+                        return answer;
+                    }
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
