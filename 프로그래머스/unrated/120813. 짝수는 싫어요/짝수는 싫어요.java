a172a571da2fbba9d69d3fe03f886d032b507a45
SoMin <thals0107@naver.com>
2023-11-29 17:11:01 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        if (n % 2 == 0) {
+            answer = new int[n/2];
+        } else {
+            answer = new int[n/2 + 1];
+        }
+        int j = 0;
+        for(int i=0; i<=n; i++){
+            if (i % 2 != 0){
+                answer[j] = i;
+                j++;
+            }
+        }
+        return answer;
+    }
+}
