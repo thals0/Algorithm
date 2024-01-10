18490c2cc608f177115819a5ca57a2162beb6b9c
SoMin <thals0107@naver.com>
2024-01-10 15:00:53 +0900

    [level 3] Title: 등굣길, Time: 1.66 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 3.77 ms
+메모리: 10.2 MB, 시간: 1.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 13:46:16
+2024년 1월 3일 15:0:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,12 +1,25 @@
 def solution(m, n, puddles):
-    dp = [[0]*(m+1) for _ in range(n+1)]
-    dp[1][1] = 1
+    ans = 0
+    arr = [[1] * m for _ in range(n)]
+    for x, y in puddles:
+        arr[y-1][x-1] = 0
+    
+    for i in range(m):
+        if arr[0][i] == 0:
+            for j in range(i+1, m):
+                arr[0][j] = 0
+            break
+    for i in range(n):
+        if arr[i][0] == 0:
+            for j in range(i+1, n):
+                arr[j][0] = 0
+            break
 
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            if [j,i] in puddles: continue
-            if i-1 >=1 and [j,i-1] not in puddles:
-                dp[i][j] += dp[i-1][j]
-            if j-1 >= 1 and [j-1,i] not in puddles:
-                dp[i][j] += dp[i][j-1]
-    return dp[-1][-1] % 1000000007
+    x = 1_000_000_007
+    for i in range(1, n):
+        for j in range(1, m):
+            if arr[i][j] != 0:
+                arr[i][j] = (arr[i-1][j] % x) + (arr[i][j-1] % x)
+    
+    ans = arr[n-1][m-1] % x
+    return ans
